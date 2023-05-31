8927a8b3722d72b8f4c26154c42e1f6f74b4f099
philipmagar <133074456+philipmagar@users.noreply.github.com>
2023-05-31 16:47:27 +0545

    Add files via upload

---
+++ bisection method.cpp
@@ -0,0 +1,44 @@
+//Bisection Method to find the root of a non – linear equation correct upto 3 decimal places
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define f(x) pow(x,2)*sin(x)+ exp(-x)-3
+void main()
+{
+	 float x0, x1, x2, f0, f1, f2, e;
+	 int step = 1;
+	 up:
+	 printf("\nEnter two initial guesses:\n");
+	 scanf("%f%f", &x0, &x1);
+	 printf("Enter tolerable error:\n");
+	 scanf("%f", &e);
+	 f0 = f(x0);
+	 f1 = f(x1);
+	 if( f0 * f1 > 0.0)
+	 {
+		  printf("Incorrect Initial Guesses.\n");
+		  goto up;
+	 }
+	 printf("\nStep\t\tx0\t\tx1\t\tx2\t\tf(x2)\n");
+	 do
+	 {
+		  x2 = (x0 + x1)/2;
+		  f2 = f(x2);
+		
+		  printf("%d\t\t%f\t%f\t%f\t%f\n",step, x0, x1, x2, f2);
+		
+		  if( f0 * f2 < 0)
+		  {
+			   x1 = x2;
+			   f1 = f2;
+		  }
+		  else
+		  {
+			   x0 = x2;
+			   f0 = f2;
+		  }
+		  step = step + 1;
+	 }while(fabs(f2)>e);
+	 printf("\nRoot is: %f", x2);
+	 getch();
+}
